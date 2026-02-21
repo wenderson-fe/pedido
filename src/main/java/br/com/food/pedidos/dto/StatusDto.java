a040af8858471736de36a6ba5f1664fe1339ec74
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2026-02-20 23:11:39 -0300

    refactor(pedido): converte StatusDto para record

--- src/main/java/br/com/food/pedidos/dto/StatusDto.java
+++ src/main/java/br/com/food/pedidos/dto/StatusDto.java
@@ -1,15 +1,8 @@
 package br.com.food.pedidos.dto;
 
 import br.com.food.pedidos.model.Status;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class StatusDto {
-    private Status status;
+public record StatusDto(
+        Status status
+) {
 }
