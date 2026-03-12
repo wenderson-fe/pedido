4ab373ad76a22e766f25cf853dc67e87b4132479
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2026-03-12 03:57:58 -0300

    chore(config): adicionar dependências e configurações do OpenFeign e Resilience4j

--- src/main/java/br/com/food/pedidos/PedidosApplication.java
+++ src/main/java/br/com/food/pedidos/PedidosApplication.java
@@ -2,8 +2,10 @@ package br.com.food.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PedidosApplication {
 
 	public static void main(String[] args) {
